f68de2c54d40447f763f9af5f3939c83c30237b4
Tiago <tiagooliveiraaso2@gmail.com>
2022-04-23 17:32:30 -0300

    components load

---
+++ resources/views/components/load-page.blade.php
@@ -0,0 +1,4 @@
+<div id="{{$id}}" class="load-page">
+    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
+    <img src="{{asset('img/load-page.gif')}}" alt="" class="img-fluid">
+</div>
