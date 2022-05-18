a6d0a4ad5b4317a5b23fb7edfd3e41b9541a8719
Tiago <tiagooliveiraaso2@gmail.com>
2022-05-17 22:49:14 -0300

    toast e try catch adcionado na alteração de status agendamento

--- app/Http/Livewire/Components/AgendarCliente/Table.php
+++ app/Http/Livewire/Components/AgendarCliente/Table.php
@@ -5,11 +5,18 @@ namespace App\Http\Livewire\Components\AgendarCliente;
 use Livewire\Component;
 use App\Models\Agendamento;
 use App\Http\Classes\Configuracao;
-
+new Configuracao();
 class Table extends Component
 {
     public $agendamento_id = 0;
     public $status_agendamento = "";
+    public $toast_type = ['success' => 0,'info' => 1,'warning' => 2,'error' => 3];
+    public $msg_toast = [
+        "title" => '',
+        "information" => '',
+        "type" => 1,
+        "time" => TIME_TOAST
+    ];
     protected $listeners = [
         'agendamentos-reload' => '$refresh',
         'editStatus'
@@ -23,27 +30,48 @@ class Table extends Component
     public function openModalEditStatus($agendamento_id)
     {
         # code...
-        $this->agendamento_id = $agendamento_id;
-        $agedamento = Agendamento::find($agendamento_id);
-        $this->status_agendamento = $agedamento->status_agendamento;
-        $this->emit('components.agendar-cliente.table_openModal');
+        try {
+            $this->agendamento_id = $agendamento_id;
+            $agedamento = Agendamento::find($agendamento_id);
+            $this->status_agendamento = $agedamento->status_agendamento;
+            $this->emit('components.agendar-cliente.table_openModal');
+        } catch (\Exception $e) {
+            $this->msg_toast['title'] = 'Erro!';
+            $this->msg_toast['information'] = $e->getMessage();
+            $this->msg_toast['type'] = $this->toast_type['error'];
+            $this->emit('showToast', $this->msg_toast);
+        }
     }
 
     public function showQuestionYesNo()
     {
-        $status = Configuracao::getOpcoesStatusAgendamento();
-        $this->emit('components.agendar-cliente.table_closeModal');
-        $this->emit('components.agendar-cliente.table_showQuestionYesNo', "Deseja mesmo mudar o status do agendamento para: <span class='text-primary'>".$status[$this->status_agendamento]."</span>");
+        try {
+            $status = Configuracao::getOpcoesStatusAgendamento();
+            $this->emit('components.agendar-cliente.table_closeModal');
+            $this->emit('components.agendar-cliente.table_showQuestionYesNo', "Deseja mesmo mudar o status do agendamento para: <span class='text-primary'>".$status[$this->status_agendamento]."</span>");
+        } catch (\Exception $e) {
+            $this->msg_toast['title'] = 'Erro!';
+            $this->msg_toast['information'] = $e->getMessage();
+            $this->msg_toast['type'] = $this->toast_type['error'];
+            $this->emit('showToast', $this->msg_toast);
+        }
     }
 
     public function editStatus()
     {
         # code...
-        Agendamento::where('id', $this->agendamento_id)->update([
-            'status_agendamento' => $this->status_agendamento
-        ]);
-        $this->emit('agendamentos-reload');
-        $this->emit('components.agendar-cliente.table_closeModal');
+        try {
+            Agendamento::where('id', $this->agendamento_id)->update([
+                'status_agendamento' => $this->status_agendamento
+            ]);
+            $this->emit('agendamentos-reload');
+            $this->emit('components.agendar-cliente.table_closeModal');
+        } catch (\Exception $e) {
+            $this->msg_toast['title'] = 'Erro!';
+            $this->msg_toast['information'] = $e->getMessage();
+            $this->msg_toast['type'] = $this->toast_type['error'];
+            $this->emit('showToast', $this->msg_toast);
+        }
     }
 
     public function render()
